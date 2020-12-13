0e92c562d0f703dd2ef3728ac38131771addd4b3
Russell Tran <tranrl@stanford.edu>
2020-12-12 22:33:06 -0800

    Decompose code, convert all tabs to spaces

--- script.py
+++ script.py
@@ -35,7 +35,7 @@ import watchtower
 
 def upload_file(s3_client, file_name, bucket, object_name):
     """Upload a file to an S3 bucket
-	:param s3_client: Initialized S3 client to use
+    :param s3_client: Initialized S3 client to use
     :param file_name: File to upload
     :param bucket: Bucket to upload to
     :param object_name: S3 object name. Also known as a "Key" in S3 bucket terms.
@@ -50,38 +50,38 @@ def upload_file(s3_client, file_name, bucket, object_name):
     return True
 
 def generate_bucket_key(path):
-	"""Keep things nice and random to prevent collisions
-	"/Users/russell/Documents/taco_tuesday.jpg" becomes "taco_tuesday-b94b0793-6c74-44a9-94e0-00420711130d.jpg"
-	"""
-	root_ext = os.path.splitext(ntpath.basename(path));
-	return root_ext[0] + "-" + str(uuid.uuid4()) + root_ext[1];
+    """Keep things nice and random to prevent collisions
+    "/Users/russell/Documents/taco_tuesday.jpg" becomes "taco_tuesday-b94b0793-6c74-44a9-94e0-00420711130d.jpg"
+    """
+    root_ext = os.path.splitext(ntpath.basename(path));
+    return root_ext[0] + "-" + str(uuid.uuid4()) + root_ext[1];
 
 def ensure_today_subdir(dir):
-	"""If dir doesn't have a subdirectory named today's date in "YYYY-MM-DD" format,
-	make one and return the path
-	"""
-	today_subdir = os.path.join(dir, datetime.today().strftime('%Y-%m-%d'))
-	if not os.path.exists(today_subdir):
-    	os.makedirs(today_subdir)
+    """If dir doesn't have a subdirectory named today's date in "YYYY-MM-DD" format,
+    make one and return the path
+    """
+    today_subdir = os.path.join(dir, datetime.today().strftime('%Y-%m-%d'))
+    if not os.path.exists(today_subdir):
+        os.makedirs(today_subdir)
     return today_subdir
 
 def move(src_path, dir):
-	""" Move the file {src_path} to {dir}/YYYY-MM-DD/{src_path's basename},
-	where YYYY-MM-DD is today's date. If the YYYY-MM-DD subdirectory doesn't exist, creates it.
-	If the destination path is already taken, moves the file to 
-	"{dir}/YYYY-MM-DD/{src_path's baseroot} (i){src ext}" instead to avoid collisions. 
-	(Where i is a recursive increment).
-	"""
-	today_subdir = ensure_today_subdir(dir)
-	dst_path = os.path.join(today_subdir, ntpath.basename(src_path))
-	# Avoid collisions
-	i = 0
-	while os.path.isfile(dst_path):
-    	i += 1
-    	root_ext = os.path.splitext(dst_path)
-    	dst_path = root_ext[0] + " ({})".format(i) + root_ext[1]
+    """ Move the file {src_path} to {dir}/YYYY-MM-DD/{src_path's basename},
+    where YYYY-MM-DD is today's date. If the YYYY-MM-DD subdirectory doesn't exist, creates it.
+    If the destination path is already taken, moves the file to 
+    "{dir}/YYYY-MM-DD/{src_path's baseroot} (i){src ext}" instead to avoid collisions. 
+    (Where i is a recursive increment).
+    """
+    today_subdir = ensure_today_subdir(dir)
+    dst_path = os.path.join(today_subdir, ntpath.basename(src_path))
+    # Avoid collisions
+    i = 0
+    while os.path.isfile(dst_path):
+        i += 1
+        root_ext = os.path.splitext(dst_path)
+        dst_path = root_ext[0] + " ({})".format(i) + root_ext[1]
     # Finally move file
-	shutil.move(src_path, dst_path)
+    shutil.move(src_path, dst_path)
 
 class ProgressPercentage(object):
 
@@ -91,43 +91,77 @@ class ProgressPercentage(object):
         self._seen_so_far = 0
 
     def __call__(self, bytes_amount):
-    	"""Callback that logs how many bytes have been uploaded so far for a particular file
-    	"""
+        """Callback that logs how many bytes have been uploaded so far for a particular file
+        """
         self._seen_so_far += bytes_amount
         percentage = (self._seen_so_far / self._size) * 100
         logging.info("Uploading status for {}: {} / {} ({}%)"
-        	.format(self._filename, self._seen_so_far, self._size, percentage))
+            .format(self._filename, self._seen_so_far, self._size, percentage))
 
 class S3EventHandler(FileSystemEventHandler):
 
-	def __init__(self, s3_client, s3_bucket, unprocessed_dir, done_dir, error_dir):
-		self.s3_client = s3_client
-		self.s3_bucket = s3_bucket
-		self.unprocessed_dir = unprocessed_dir
-		self.done_dir = done_dir
-		self.error_dir = error_dir
+    def __init__(self, s3_client, s3_bucket, unprocessed_dir, done_dir, error_dir):
+        self.s3_client = s3_client
+        self.s3_bucket = s3_bucket
+        self.unprocessed_dir = unprocessed_dir
+        self.done_dir = done_dir
+        self.error_dir = error_dir
 
     def on_created(self, event):
-    	if not event.is_directory:
-    		file_path = event.src_path
-    		object_name = generate_bucket_key(file_path)
-        	success = upload_file(self.s3_client, file_path, self.s3_bucket, object_name,
-        		Callback=ProgressPercentage(file_path))
-        	if success:
-        		move(file_path, self.done_dir)
-        	else:
-        		move(file_path, self.error_dir)
+        if not event.is_directory:
+            file_path = event.src_path
+            object_name = generate_bucket_key(file_path)
+            success = upload_file(self.s3_client, file_path, self.s3_bucket, object_name,
+                Callback=ProgressPercentage(file_path))
+            if success:
+                move(file_path, self.done_dir)
+            else:
+                move(file_path, self.error_dir)
+
+def get_preexisting_files(dir):
+    preexisting = []
+    for root, dirs, files in os.walk(dir):
+        files = [f for f in files if not f[0] == '.']
+        for file in files:
+            preexisting.append(os.path.join(root, file))
+    return preexisting
+
+def process_preexisting_files(s3_client, bucket, preexisting):
+    for file_path in preexisting:
+        object_name = generate_bucket_key(file_path)
+        success = upload_file(s3_client, file_path, bucket, object_name,
+            Callback=ProgressPercentage(file_path))
+        if success:
+            move(file_path, done_dir)
+        else:
+            move(file_path, error_dir)
+
+def keep_running():
+    """Note: hangs until Ctrl-C
+    """
+    try:
+        s = 0
+        while True:
+            time.sleep(1)
+            s += 1
+            if config["send_heartbeat"] and s >= config["heartbeat_seconds"]:
+                logging.info("HEARTBEAT")
+                s = 0
+    except KeyboardInterrupt:
+        logging.info("KeyboardInterrupt: shutting down script.py...")
+        observer.stop()
+        observer.join()
 
 def main():
-	logging.basicConfig(level=logging.INFO,
-						format='%(asctime)s - %(message)s',
-						datefmt='%Y-%m-%d %H:%M:%S')
-	config = yaml.safe_load(open("config.yml"))
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    config = yaml.safe_load(open("config.yml"))
     s3_client = boto3.client('s3', aws_access_key_id=config["s3"]["access_key"], 
-    					aws_secret_access_key=config["s3"]["secret_key"])
+                        aws_secret_access_key=config["s3"]["secret_key"])
     cloudwatch = Session(aws_access_key_id=config["cloudwatch"]["access_key"],
-    					aws_secret_access_key=config["cloudwatch"]["secret_key"],
-    					region_name=config["cloudwatch"]["region_name"])
+                        aws_secret_access_key=config["cloudwatch"]["secret_key"],
+                        region_name=config["cloudwatch"]["region_name"])
     bucket = config["s3"]["bucket"]
     unprocessed_dir = config["unprocessed_dir"]
     done_dir = config["done_dir"]
@@ -138,41 +172,19 @@ def main():
     logger.addHandler(watchtower.CloudWatchLogHandler(cloudwatch))
 
     # Check for any preexisting files in the "unprocessed" folder
-	preexisting = []
-	for root, dirs, files in os.walk(unprocessed_dir):
-    	files = [f for f in files if not f[0] == '.']
-    	for file in files:
-    		preexisting.append(os.path.join(root, file))
+    preexisting = get_preexisting_files(unprocessed_dir)
 
     # Setup the watchdog handler for new files that are added while the script is running
     event_hander = S3EventHandler(s3_client, bucket, unprocessed_dir, done_dir, error_dir)
     observer = Observer()
-	observer.schedule(event_handler, unprocessed_dir, recursive=True)
-	observer.start()
+    observer.schedule(event_handler, unprocessed_dir, recursive=True)
+    observer.start()
 
-	# Upload & process any preexisting files
-	for file_path in preexisting:
-		object_name = generate_bucket_key(file_path)
-    	success = upload_file(s3_client, file_path, bucket, object_name,
-    		Callback=ProgressPercentage(file_path))
-    	if success:
-    		move(file_path, done_dir)
-    	else:
-    		move(file_path, error_dir)
+    # Upload & process any preexisting files
+    process_preexisting_files(s3_client, bucket, preexisting)
 
     # Keep the main thread running so watchdog handler can be still be called
-	try:
-		s = 0
-		while True:
-			time.sleep(1)
-			s += 1
-			if config["send_heartbeat"] and s >= config["heartbeat_seconds"]:
-				logging.info("HEARTBEAT")
-				s = 0
-	except KeyboardInterrupt:
-		logging.info("KeyboardInterrupt: shutting down script.py...")
-		observer.stop()
-		observer.join()
+    keep_running()
 
 if __name__ == "__main__":
-	main()
+    main()
